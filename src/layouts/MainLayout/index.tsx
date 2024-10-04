6b3c0d934da41e50cff3c01959e903f455dc5013
Arman Arakelyan <arman.arakelyan.js@gmail.com>
2024-10-04 18:40:28 +0400

    feat: implement Sidebar

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -5,11 +5,16 @@ interface MainLayout {
   children: React.ReactNode | React.ReactNode[] | React.ReactElement
 }
 
+const MOCK_SIDEBAR_ITEMS = [
+  { label: 'Home', path: '#' },
+  { label: 'About', path: '#' }
+]
+
 const MainLayout = ({ children }: MainLayout) => {
   return (
     <div className="flex h-screen">
-      <Sidebar />
-      <div className="flex flex-1 flex-col">
+      <Sidebar items={MOCK_SIDEBAR_ITEMS} />
+      <div className="flex w-3/4 flex-1 flex-col ">
         <Header />
         <Main>{children}</Main>
         <Footer />
--- src/layouts/components/Sidebar/index.tsx
+++ src/layouts/components/Sidebar/index.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { Button } from '../../../components'
 
-export function Sidebar() {
+interface SidebarItem {
+  label: string
+  path: string
+}
+
+interface SidebarProps {
+  items: SidebarItem[]
+}
+
+export function Sidebar({ items }: SidebarProps) {
   return (
-    <aside className="w-1/4 shrink-0 bg-gray-700 p-4 text-white">sidebar</aside>
+    <div className="flex h-screen w-64 flex-col bg-gray-300 p-4 shadow-lg">
+      <ul className="flex-1 space-y-2">
+        {items.map((item, index) => (
+          <li key={index}>
+            <a
+              href={item.path}
+              className="flex items-center rounded-lg p-2 transition-all duration-300 hover:bg-gray-200"
+            >
+              <span className="text-gray-700">{item.label}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <Button label="Sign out" type="secondary" />
+    </div>
   )
 }
