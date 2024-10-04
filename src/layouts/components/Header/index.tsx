871ac145488f1e07a8d2bc2cf9c3244e28d6da3d
Arman Arakelyan <arman.arakelyan.js@gmail.com>
2024-10-04 18:40:13 +0400

    feat: implement Header

--- src/layouts/components/Header/index.tsx
+++ src/layouts/components/Header/index.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import { Button } from '../../../components'
 
 export function Header() {
-  return <header className="bg-gray-200 p-4">header</header>
+  return (
+    <header className="flex justify-end bg-gray-200 p-4">
+      <Button label="My account" type="secondary" />
+    </header>
+  )
 }
